a91043239bfc337464e6d560d45d0877ad64b0d3
SantiGO055 <santigonzalez05@gmail.com>
2024-02-08 20:50:08 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Santiago Gonzalez on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
