f54f536ad4d1777cbb4ecd1c1d7f698d93f1fd0a
SantiGO055 <santigonzalez05@gmail.com>
2024-02-08 22:20:06 -0300

    adding 3d animation x y z

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -13,24 +13,16 @@ struct ContentView: View {
     
     var body: some View {
 
-        print(animationAmount)
-        return VStack {
-                    Stepper("Scale amount", value: $animationAmount.animation(
-                        .easeInOut(duration: 1)
-                            .repeatCount(3, autoreverses: true)
-                    ), in: 1...10)
-
-                    Spacer()
-
-                    Button("Tap Me") {
-                        animationAmount += 1
+        Button("Tap Me") {
+                    withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                        animationAmount += 360
                     }
-                    .padding(40)
-                    .background(.red)
-                    .foregroundStyle(.white)
-                    .clipShape(.circle)
-                    .scaleEffect(animationAmount)
                 }
+                .padding(50)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
