87068911e0555a39358a27140b151d56b1e88634
SantiGO055 <santigonzalez05@gmail.com>
2024-02-10 14:09:33 -0300

    frame animate with movement of tap finger

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,28 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-                enabled.toggle()
-            }
-            
-            .frame(width: 200, height: 200)
-            .background(enabled ? .blue : .red)
-            .animation(nil, value: enabled)
-            .foregroundStyle(.white)
-            .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-            .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
-            
-        
-            
-            
+            LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .clipShape(.rect(cornerRadius: 10))
+                .offset(dragAmount)
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            withAnimation(.bouncy) {
+                                dragAmount = .zero
+                            }
+                        }
+                )
+                .animation(.bouncy, value: dragAmount)
         }
-        .padding()
-        
-    }
 }
 
 #Preview {
